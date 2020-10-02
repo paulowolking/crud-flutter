efcec7d870ab3e78c699abf56b13a5e9972608ea
Paulo <phc25@hotmail.com>
2020-10-01 22:44:42 -0300

    Subindo projeto.

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: "1",
+      name: 'Maria',
+      email: 'maria@email.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2016/08/20/05/36/avatar-1606914_960_720.png'),
+  '2': const User(
+      id: "2",
+      name: 'Fernanda',
+      email: 'fernanda@email.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2016/03/31/20/27/avatar-1295773_960_720.png'),
+  '3': const User(
+      id: "3",
+      name: 'Carlos',
+      email: 'carlos@email.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_960_720.png'),
+  '4': const User(
+      id: "4",
+      name: 'Thaís',
+      email: 'thais@email.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png')
+};
---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_crud/provider/users_provider.dart';
+import 'package:flutter_crud/routes/app_routes.dart';
+import 'package:flutter_crud/views/user_form.dart';
+import 'package:flutter_crud/views/user_list.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => UsersProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: UserList(),
+        routes: {
+          AppRoutes.HOME: (_) => UserList(),
+          AppRoutes.USER_FORM: (_) => UserForm()
+        },
+      ),
+    );
+
+    // return ChangeNotifierProvider(
+    //   create: (ctx) => Users(),
+    //   child: MaterialApp(
+    //     title: 'Flutter Demo',
+    //     theme: ThemeData(
+    //       primarySwatch: Colors.blue,
+    //       visualDensity: VisualDensity.adaptivePlatformDensity,
+    //     ),
+    //     home: UserList(),
+    //   ),
+    // );
+  }
+}
